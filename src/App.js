8fc0b837ff326b0261e640d43ff93b468ac1bd65
Jimmy Svensson <jimmy.svensson@nethouse.se>
2020-10-12 15:33:22 +0200

    faq and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { FaqsContainer } from "./containers/faq";
+import { FooterContainer } from "./containers/footer";
 import { JumbotronContainer } from "./containers/jumbotron";
 
-//https://github.com/karlhadwen/netflix/tree/master/src
+//https://github.com/karlhadwen/netflix
+//https://www.youtube.com/watch?v=x_EEwGe-a9o&t=4265s&ab_channel=KarlHadwen
+
 export default function App() {
-  return <JumbotronContainer></JumbotronContainer>;
+  return (
+    <>
+      <JumbotronContainer></JumbotronContainer>
+      <FaqsContainer></FaqsContainer>
+      <FooterContainer></FooterContainer>
+    </>
+  );
 }
