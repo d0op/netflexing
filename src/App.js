0b97acb0e7d1837b5d3dccedaae2783347c773cf
Jimmy Svensson <jimmy.svensson@nethouse.se>
2020-10-13 13:26:02 +0200

    pretty compelted

--- src/App.js
+++ src/App.js
@@ -2,26 +2,41 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Browse, Home, SignIn, SignUp } from "./pages";
+import { useAuthListener } from "./hooks";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 //https://github.com/karlhadwen/netflix
 //https://www.youtube.com/watch?v=x_EEwGe-a9o&t=4265s&ab_channel=KarlHadwen
 
 export default function App() {
+  const { user } = useAuthListener();
   return (
     <Router>
       <Switch>
-        <Route exact path={ROUTES.HOME}>
-          <Home></Home>
-        </Route>
-        <Route exact path={ROUTES.SIGN_UP}>
-          <SignUp></SignUp>
-        </Route>
-        <Route exact path={ROUTES.SIGN_IN}>
-          <SignIn></SignIn>
-        </Route>
-        <Route exact path={ROUTES.BROWSE}>
-          <Browse></Browse>
-        </Route>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+        >
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
       </Switch>
     </Router>
   );
