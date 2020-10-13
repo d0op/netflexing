a94e624a64ffdc8de833df8fa53a41a6ae546d77
Jimmy Svensson <jimmy.svensson@nethouse.se>
2020-10-13 11:00:34 +0200

    finihsed homepage
    homepage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faq";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Browse, Home, SignIn, SignUp } from "./pages";
 
 //https://github.com/karlhadwen/netflix
 //https://www.youtube.com/watch?v=x_EEwGe-a9o&t=4265s&ab_channel=KarlHadwen
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer></JumbotronContainer>
-      <FaqsContainer></FaqsContainer>
-      <FooterContainer></FooterContainer>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.HOME}>
+          <Home></Home>
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <SignUp></SignUp>
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <SignIn></SignIn>
+        </Route>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse></Browse>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
